aa25a3182a6577dae6b22622d114cad52717460c
tanya <tanyarags@gmail.com>
2013-01-11 01:38:38 +0530

    Sort Working Correctly

--- Final/src/Final.cpp
+++ Final/src/Final.cpp
@@ -25,10 +25,6 @@ int thread_stop = 0;
 
 FILE *file[FILE_NUMBER];
 
-void openFile(string name, int thread_number);
-void createFile(string name, int thread_number);
-void writeToFile(int data, int file_number);
-
 
 
 void *SortInFile(void *threadid) {
@@ -42,15 +38,16 @@ void *SortInFile(void *threadid) {
 	out << tid;
 	file_name = out.str();
 	file_name = file_name + "_file";
-	openFile(file_name, tid);
+	file[tid] = fopen(file_name.c_str(), "r");
+
 
 	while (fscanf(file[tid], "%d\n", &n[i]) == 1)
-	    {
-		cout<<n[i];
-		i++;
-	    }
+     i++;
+	fclose(file[tid]);
 
-	int temp;
+	//cout<<"File closed";
+	  int temp;
+
 	//sorting here with simple bubble sort
 	for(int k=0;k<200;k++)
 	for(int j=0;j<(200-k);j++)
@@ -60,13 +57,15 @@ void *SortInFile(void *threadid) {
 	n[j]=n[j+1];
 	n[j+1]=temp;
 	}
-
-	createFile(file_name, tid);
-	rewind(file[tid]);
-	for (int i = 0; i < NUMBER_NUMBER; i++) {
-			writeToFile(n[i], tid);
+	for(int i=0;i<200;i++)
+		{
+		cout<<n[i]<<"\n";
 		}
 
+	file[tid] = fopen(file_name.c_str(), "w");
+	for(int i=0;i<200;i++)
+	fprintf(file[tid], "%d%s", n[i], "\n");
+	fclose(file[tid]);
 
 
 	pthread_exit(NULL);
@@ -74,56 +73,26 @@ void *SortInFile(void *threadid) {
 
 
 
-void *WriteFileThread(void *threadid) {
-	int tid;
-	tid = (int) threadid;
-	cout << "Started thread" << tid << endl;
-	string file_name;
-	stringstream out;
-	out << tid;
-	file_name = out.str();
-	file_name = file_name + "_file";
-	file[thread_number] = fopen(file_name.c_str(), "r");
-	rewind(file[tid]);
-	for (int i = 0; i < NUMBER_NUMBER; i++) {
-		int number = rand() % 100;
-		writeToFile(number, tid);
-	}
-	pthread_exit(NULL);
-}
 
 
 int main (int argc, char *argv[])
 {
    pthread_t threads[NUM_THREADS];
-   int rc;
    int sort;
    long t;
-   for(t=0; t<NUM_THREADS; t++){
-      //printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, WriteFileThread, (void *)t);
-      if (rc){
-         //printf("ERROR; return code from pthread_create() is %d\n", rc);
-         exit(-1);
-      }
-   }
-   for(t=0; t<NUM_THREADS; t++){
-	   pthread_join(threads[t], NULL);
-   }
-
-
    cout<< "Here";
-   for(t=0; t<NUM_THREADS; t++){
-         //printf("In main: creating thread %ld\n", t);
+
+   // here the sorting starts
+   for(t=0; t<NUM_THREADS; t++)
+         {
          sort = pthread_create(&threads[t], NULL, SortInFile, (void *)t);
-         if (sort){
-            //printf("ERROR; return code from pthread_create() is %d\n", rc);
-            exit(-1);
-         }
-      }
-      for(t=0; t<NUM_THREADS; t++){
-   	   pthread_join(threads[t], NULL);
-      }
+           if (sort)
+           exit(-1);
+          }
+
+           for(t=0; t<NUM_THREADS; t++){
+   	       pthread_join(threads[t], NULL);
+         } // here the sorting ends
 
 
    /* Last thing that main() should do */
@@ -134,17 +103,6 @@ int main (int argc, char *argv[])
 
 
 
-void createFile(string name, int thread_number) {
-	file[thread_number] = fopen(name.c_str(), "w");
-}
-
-void openFile(string name, int thread_number) {
-	file[thread_number] = fopen(name.c_str(), "r");
-}
-
-void writeToFile(int data, int file_number) {
-	fprintf(file[file_number], "%d%s", data, "\n");
-}
 
 
 
